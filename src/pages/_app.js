18f4807ee1ce14920274c9136e5970bac6080ced
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2023-05-30 22:57:19 +1000

    env typo error fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,14 +11,14 @@ export default function App({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT.NEXT_PUBLIC_GA_ID}`}
         strategy="afterInteractive"
       />
-  
-      <Script id="google-analytics-script">
+
+      <Script id="google-analytics-script" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', '${process.env.NEXT.NEXT_PUBLIC_GA_ID}')`}
-        ;
+  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}')`}
+        
       </Script>
       <Layout>
         <Component {...pageProps} />
