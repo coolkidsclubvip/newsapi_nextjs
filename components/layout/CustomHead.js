98403d5d5f99b3c406a1b2abf8a22dbd0aab66a4
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2023-06-01 22:36:59 +1000

    api key updated again

--- components/layout/CustomHead.js
+++ components/layout/CustomHead.js
@@ -2,30 +2,32 @@ import Head from "next/head";
 
 function CustomHead(props) {
   return (
-    <Head>
-      <title>The Nuts News</title>
-      <meta
-        name="keywords"
-        content="local news, local weather, australian news, world news"
-      />
-      <meta
-        name="description"
-        content="Nuts News offers a wide range of latest Australian and international news from different news sources."
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <>
+      <Head>
+        <title>The Nuts News</title>
+        <meta
+          name="keywords"
+          content="local news, local weather,australian news,world news"
+        />
+        <meta
+          name="description"
+          content="Nuts News offers a wide range of latest australian and international news from different news sources"
+        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
-        media="print"
-        onload="this.media='all'"
-      />
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
-        defer
-      ></script>
-    </Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
+
+        <title>{`Nuts News | ${props.title}`}</title>
+      </Head>
+    </>
   );
 }
 
