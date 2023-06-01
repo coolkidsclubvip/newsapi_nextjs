3a0db9cdad47d4d5683b839b8b81e3d70784bca0
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2023-06-01 18:35:41 +1000

    Set carousel image priority for LCP

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -24,6 +24,7 @@ function HeroSection({ articles2 }) {
                     layout="responsive"
                     width={1200}
                     height={675}
+                    priority // Optional: Set priority to preload the image
                   />
                   <div className="carousel-caption  d-md-block">
                     <div className={styles.hero_caption}>
