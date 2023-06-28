b17eb0737eeba38391cd45427294b77fe8cd3d51
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2023-06-28 12:46:43 +1000

    new key

--- components/fetch/fetchAuArticles.js
+++ components/fetch/fetchAuArticles.js
@@ -1,6 +1,6 @@
 async function fetchAuArticles() {
   const res = await fetch(
-    `https://newsapi.org/v2/everything?sources=abc-news-au&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY1}`
+    `https://newsapi.org/v2/everything?sources=abc-news-au&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
   );
   const data = await res.json();
   const articles = data.articles;
