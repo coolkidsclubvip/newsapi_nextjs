b8ef4decece57efd32280ad17446a35d8eab52e2
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2024-04-18 22:33:03 +1000

    new coin APIkey& pagi scroll to top  condition

--- components/mapping/ArticlesList/ArticlesList.js
+++ components/mapping/ArticlesList/ArticlesList.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 import styles from "./ArticleList.module.scss";
 import ArticleItem from "../ArticleItem/ArticleItem";
+import { useRouter } from "next/router";
 
 function ArticlesList({ articles1, category }) {
-  // React-paginate is below:
+  const [isHomePage, setIsHomePage] = useState(false);
 
+  // React-paginate is below:
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 15;
   // Simulate fetching items from another resources.
@@ -16,16 +18,32 @@ function ArticlesList({ articles1, category }) {
   const currentItems = articles1.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(articles1.length / itemsPerPage);
 
+  const router = useRouter();
+  // 判断当前页面是否是主页
+  useEffect(() => {
+    setIsHomePage(router.pathname === "/");
+    
+  }, []);
+
   // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % articles1.length;
 
     setItemOffset(newOffset);
+
+    // 判断是否为首页
+    if (isHomePage) {
+      // 如果是首页，滚动到顶部下方 110vh 的位置
+      window.scrollTo({ top: window.innerHeight * 1.1, behavior: "smooth" });
+    } else {
+      // 如果不是首页，滚动到页面顶部
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   };
 
   // Last current page articles' index number is:
   const lastIndex = currentItems.length - 1;
-
+console.log("isHomepage is:", isHomePage);
   return (
     <div className="container-fluid p-0">
       <div className={styles.articlesList}>
