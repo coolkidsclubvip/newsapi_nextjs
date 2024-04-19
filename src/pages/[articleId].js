a9a089190a9a88b11a28ffa050c08411b07fa160
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2024-04-19 23:45:15 +1000

    Check if articleMatch is empty

--- src/pages/[articleId].js
+++ src/pages/[articleId].js
@@ -2,18 +2,15 @@ import { Fragment, useState } from "react";
 import CustomHead from "../../components/layout/CustomHead";
 import ArticleDetail from "../../components/ArticleDetail/ArticleDetail";
 import getCombinedArticles from "../../components/fetch/getCombinedArticles";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Loader from "../../components/Loader/index";
 
 const ArticleId = (props) => {
-
- 
-
-  const router = useRouter()
-  if(router.isFallback) {
-    return (<Loader/>)
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loader />;
   }
-  
+
   const { userArticle } = props;
   const category = "";
   return (
@@ -43,29 +40,35 @@ export const getStaticPaths = async () => {
     params: { articleId: title.toString() },
   }));
 
-
   return {
     paths,
-    fallback:true,
+    fallback: true,
   };
 };
 
 // STATIC SITE GENERATION
 export const getStaticProps = async (context) => {
   const combinedArticles = await getCombinedArticles();
+  console.log("combinedArticles are:", combinedArticles);
   //  Store params id value (article USER wants!)
   const articleQuery = await context.params.articleId; // This articleId is passed in through click
   //  Filters articles array to match & return article passed in params
   const articleMatch = combinedArticles.filter(
     (article) => article.title.toString() === articleQuery
   );
+  console.log("articleMatch are:", articleMatch);
+  // Check if articleMatch is empty
+  if (articleMatch.length === 0) {
+    return {
+      notFound: true, // Return 404 page
+    };
+  }
 
- 
   return {
     props: {
       userArticle: articleMatch[0],
     },
-    revalidate: 60*60,
+    revalidate: 60 * 60,
   };
 };
 
